5912ab94a70c0cc14bd478b999a0c989f8480b34
Loic Lissens <loiclissens@gmail.com>
2025-04-08 23:15:16 +0200

    🖖
    Signed-off-by: Loic Lissens <loiclissens@gmail.com>

---
+++ src/components/utils/CodeBlock.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import hljs from 'highlight.js/lib/core';
+    import json from 'highlight.js/lib/languages/json';
+    import { onMount } from 'svelte';
+
+    export let code:string
+    let ref:HTMLElement;
+
+    onMount(() => {
+        hljs.registerLanguage('json', json);
+        hljs.highlightElement(ref);
+    });
+</script>
+<pre>
+    <code bind:this={ref} >{code}</code>
+  </pre>
